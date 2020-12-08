3a8dd584c512d489f88a1f8fd9855c4d41710795
Jesus <fernandezjodar.jesus@gmail.com>
2020-12-08 14:11:45 +0100

    PWA: Suscripción a actualizaciones

--- world-bank/apps/website/src/app/app.component.ts
+++ world-bank/apps/website/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { SwUpdate } from '@angular/service-worker';
 
 @Component({
   selector: 'wb-website-root',
@@ -7,4 +8,24 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'World Bank';
+  constructor(private swu: SwUpdate){
+    this.checkUpdatedVersion();
+  }
+
+  checkUpdatedVersion() {
+    if (this.swu.isEnabled) {
+      this.swu.checkForUpdate().then(data => console.log(data));
+      this.swu.available.subscribe(event => {
+        if (event.current.appData) {
+          const appData: any = event.current.appData;
+          let msg = `Nueva versión ${appData.version} disponible.`;
+          msg += `${appData.changelog}.`;
+          msg += '¿Recargar y actualizar?';
+          if (confirm(msg)) {
+            window.location.reload();
+          }
+        }
+      });
+    }
+  }
 }
